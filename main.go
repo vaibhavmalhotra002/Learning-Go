005f337d9602c43e2603d0ab27c7ab150ddfc9c2
vaibhavmalhotra002 <vaibhavmalhotra002@gmail.com>
2022-09-27 12:08:53 +0530

    main

--- main.go
+++ main.go
@@ -1,3 +1,42 @@
 package main
 
-func main()
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "hello!")
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "Parseform() err: %v", err)
+		return
+	}
+	fmt.Fprint(w, "POST Request Succesful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
+}
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("func", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Starting Server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	} //ListenAndServe is the function that creates our web server & is most imp called as heart of web server
+}
